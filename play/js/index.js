d9fe09077c229b9e54ebd8af4910947d68f6cb89
Ding Ye <1076943812@qq.com>
2020-09-11 17:18:10 +0800

    面试

--- play/js/index.js
+++ play/js/index.js
@@ -10,3 +10,12 @@ let player = new Player({
     height: 580,
     width: 800
 });
+// console.log((player.video).addEventListener())
+player.video.addEventListener("timeupdate", function() {
+    var timeDisplay = player.video.currentTime;
+    console.log(timeDisplay)
+})
+player.video.addEventListener('loadedmetadata', function() {
+    //设置上次播放时间lastLearnTime(秒)
+    player.video.currentTime = "20";
+});
